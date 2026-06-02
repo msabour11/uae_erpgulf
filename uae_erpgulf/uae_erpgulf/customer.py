cc6acea1cdff1ed2f42f0c1f9051b95b85cc5841
HusnaMa <husna@htsqatar.com>
2026-06-02 08:37:01 +0300

    the hint type semgrep error solved

--- uae_erpgulf/uae_erpgulf/customer.py
+++ uae_erpgulf/uae_erpgulf/customer.py
@@ -20,7 +20,6 @@ def custom_lookup_peppol_id_of_participant(company:str,peppol_id:str):
 
         if response.status_code == 200:
             return response.json()
-            
         else:
             frappe.throw(f"API Error: {response.text}")
 
